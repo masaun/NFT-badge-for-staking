279b75833093b5df25bce21a7694cc99f31bf928
masaun <masanoriuno@gmail.com>
2021-05-30 08:55:06 +0900

    [SC]: Define StakePool.sol

---
+++ contracts/StakePool.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+contract StakePool {
+
+    constructor() public {}
+
+    function stake() public returns (bool) {}
+
+    function unstake() public returns (bool) {}
+
+}
