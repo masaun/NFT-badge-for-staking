93c06c7e17b84f16fcc19bbf28f48b912c61a8f8
masaun <masanoriuno@gmail.com>
2021-05-30 09:13:00 +0900

    [SC]: Update stake() and unstake() in StakingPool.sol

--- contracts/RuleController.sol
+++ contracts/RuleController.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 
+import { LPToken } from "./LPToken.sol";
+import { RewardToken } from "./RewardToken.sol";
+import { StakingPool } from "./StakingPool.sol";
+
 import { StakingNFTBadgeFor3MonthsFactory } from "./StakingNFTBadgeFor3MonthsFactory.sol";
 import { StakingNFTBadgeFor6MonthsFactory } from "./StakingNFTBadgeFor6MonthsFactory.sol";
 import { StakingNFTBadgeFor1YearFactory } from "./StakingNFTBadgeFor1YearFactory.sol";
@@ -11,15 +15,25 @@ import { StakingNFTBadgeFor1YearFactory } from "./StakingNFTBadgeFor1YearFactory
  */
 contract RuleController {
 
+    LPToken public lpToken;
+    RewardToken public rewardToken;
+    StakingPool public stakingPool;
+
     StakingNFTBadgeFor3MonthsFactory public badgeFor3MonthsFactory;
     StakingNFTBadgeFor6MonthsFactory public badgeFor6MonthsFactory;
     StakingNFTBadgeFor1YearFactory public badgeFor1YearFactory;
 
     constructor(
+        LPToken _lpToken,
+        RewardToken _rewardToken,
+        StakingPool _stakingPool,
         StakingNFTBadgeFor3MonthsFactory _badgeFor3MonthsFactory, 
         StakingNFTBadgeFor6MonthsFactory _badgeFor6MonthsFactory, 
         StakingNFTBadgeFor1YearFactory _badgeFor1YearFactory
     ) public {
+        lpToken = _lpToken;
+        rewardToken = _rewardToken;
+        stakingPool = _stakingPool;
         badgeFor3MonthsFactory = _badgeFor3MonthsFactory;
         badgeFor6MonthsFactory = _badgeFor6MonthsFactory;
         badgeFor1YearFactory = _badgeFor1YearFactory;
@@ -30,11 +44,11 @@ contract RuleController {
      * @notice - Staking period is for 3 months
      */
     function stakeFor3Months(address to, string memory tokenURI, uint stakeAmount) public payable returns (bool) {
-        // [Todo]: TransferFrom staked-token (SNX tokens)
-        //snx.transferFromAndSettle(msg.sender, address(this), stakeAmount);
+        // Staked-token (LP tokens) is transferred from msg.sender to this contract
+        lpToken.transferFrom(msg.sender, address(this), stakeAmount);
 
-        // [Todo]: Put staking method from Synthetix's interface into here
-        //stakingRewards.stake(stakeAmount);
+        // Stake LP tokens into the StakingPool contract
+        stakingPool.stake(stakeAmount);
 
         // Distribute NFT that represent stake for 3 months
         badgeFor3MonthsFactory.createStakingNFTBadgeFor3Months(to, tokenURI);
@@ -45,11 +59,11 @@ contract RuleController {
      * @notice - Staking period is for 6 months
      */
     function stakeFor6Months(address to, string memory tokenURI, uint stakeAmount) public returns (bool) {
-        // [Todo]: TransferFrom staked-token (SNX tokens)
-        //snx.transferFromAndSettle(msg.sender, address(this), stakeAmount);
+        // Staked-token (LP tokens) is transferred from msg.sender to this contract
+        lpToken.transferFrom(msg.sender, address(this), stakeAmount);
 
-        // [Todo]: Put staking method from Synthetix's interface into here
-        //stakingRewards.stake(stakeAmount);
+        // Stake LP tokens into the StakingPool contract
+        stakingPool.stake(stakeAmount);
 
         // Distribute NFT that represent stake for 6 months
         badgeFor6MonthsFactory.createStakingNFTBadgeFor6Months(to, tokenURI);
@@ -60,11 +74,11 @@ contract RuleController {
      * @notice - Staking period is for 1 year
      */
     function stakeFor1Year(address to, string memory tokenURI, uint stakeAmount) public returns (bool) {
-        // [Todo]: TransferFrom staked-token (SNX tokens)
-        //snx.transferFromAndSettle(msg.sender, address(this), stakeAmount);
+        // Staked-token (LP tokens) is transferred from msg.sender to this contract
+        lpToken.transferFrom(msg.sender, address(this), stakeAmount);
 
-        // [Todo]: Put staking method from Synthetix's interface into here
-        //stakingRewards.stake(stakeAmount);
+        // Stake LP tokens into the StakingPool contract
+        stakingPool.stake(stakeAmount);
 
         // Distribute NFT that represent stake for 1 year
         badgeFor1YearFactory.createStakingNFTBadgeFor1Year(to, tokenURI);
