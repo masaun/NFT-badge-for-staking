32edea1307514fe77da4454d18a80f3fac52477d
masaun <masanoriuno@gmail.com>
2021-05-31 11:02:35 +0900

    [Migration]: Add code for depositing each tokens into the Fancet contract

--- migrations/9_deploy_Fancet.js
+++ migrations/9_deploy_Fancet.js
@@ -2,9 +2,20 @@ const Fancet = artifacts.require("Fancet")
 const LPToken = artifacts.require("LPToken")
 const RewardToken = artifacts.require("RewardToken")
 
-const lpToken = LPToken.address
-const rewardToken = RewardToken.address
+let LP_TOKEN = LPToken.address
+let REWARD_TOKEN = RewardToken.address
+let FANCET
 
 module.exports = async function(deployer) {
-    await deployer.deploy(Fancet, lpToken, rewardToken)
+    await deployer.deploy(Fancet, LP_TOKEN, REWARD_TOKEN)
+
+    FANCET = Fancet.address
+
+    const lpToken = await LPToken.at(LP_TOKEN)
+    const rewardToken = await RewardToken.at(REWARD_TOKEN)
+
+    /// Deposit each tokens into the Fancet contract
+    const amount = 1e8 * 1e18  /// 1 milion (all amount of total supply)
+    await lpToken.transfer(FANCET, amount)
+    await rewardToken.transfer(FANCET, amount)    
 }
